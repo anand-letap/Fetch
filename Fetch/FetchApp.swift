81fe9089ffaec4aeac043af1fd909e9309608fa1
Anand Patel <anand.letap@gmail.com>
2024-11-05 08:11:10 -0500

    Initial Commit

---
+++ Fetch/FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchApp.swift
+//  Fetch
+//
+//  Created by Anand Patel on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
